6e1ced9a420fa0a7bed0231dd6cc6adb66eaa49a
lemonzestyowo <kjhummel2025@gmail.com>
2025-06-08 22:42:51 -0600

    Update index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,12 +8,26 @@
  */
 
 import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+// import * as logger from 'firebase-functions/logger'; // Removed as unused
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+// The 'onRequest' function was marked as unused in your previous linting output.
+// To make it a functional HTTP Cloud Function, you need to export it.
+// This example exports it as 'helloWorld'.
+const helloWorldFunction =
+  onRequest(
+    (
+      request,
+      response,
+    ) => {
+      // logger.info('Hello logs!', {structuredData: true}); // Removed as unused
+      response.send("Hello from Firebase!");
+    },
+  );
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export {
+  helloWorldFunction as helloWorld,
+};
+
+// You can import and use other Genkit flows here if needed
+// import { myFlow } from './genkit-sample';
+// exports.myFlow = myFlow;
